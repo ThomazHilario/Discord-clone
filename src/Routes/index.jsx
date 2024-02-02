1a8efbb9188ec726435a83a0aa31d0b6668f61f9
Thomaz Alves <thomazhilario5@gmail.com>
2024-02-02 11:18:28 -0300

    Adicionando Link de rota ao header

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,12 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+// Imagens 
+import DiscordImg from '../assets/icons/discordIcon.png'
+import DevImg from '../assets/icons/devIcon.png'
 
 // Styled Components
-import { Container, Header } from './style'
+import { Container, Header, DiscordComponent } from './style'
 
 // Pages Components
 import Home from '../Pages/Home'
 
 export default function RoutePage(){
+
+    function handleFocus(value){
+
+        document.querySelectorAll('.icon').forEach(img => {
+
+            if(value.src === img.src){
+                value.style.borderRadius = '10px'
+            } else{
+                img.style.borderRadius = '50%'
+            }
+        })
+    }
+
     return(
         <BrowserRouter>
 
@@ -15,7 +32,13 @@ export default function RoutePage(){
 
                 {/* Header */}
                 <Header>
+                    <DiscordComponent>
+                        <img src={DiscordImg} alt='imagem icon discord' className='icon' onClick={(e) => handleFocus(e.target)}/>
 
+                        <hr />
+                    </DiscordComponent>
+
+                    <Link to='/'><img src={DevImg} alt='icon dev' className='icon' onClick={(e) => handleFocus(e.target)}/></Link>
                 </Header>
     
                 {/* Onde as rotas serao renderizadas */}
--- src/Routes/style.jsx
+++ src/Routes/style.jsx
@@ -11,5 +11,49 @@ export const Header = styled.header`
     width:9vh;
     height:100%;
     Background-color:rgb(32,32,32);
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    gap:8px;
+
+    img{
+        border-radius:50%;
+        height:50px;
+        width:50px;
+        object-fit:cover;
+        cursor:pointer;
+        transition:0.7s;
+
+        &:hover{
+            border-radius:10px;
+        }
+    }
+    
+`
+
+export const DiscordComponent = styled.div`
+    display:flex;
+    flex-direction:column;
+    gap:8px;
+    margin-top:5px;
+
+    img{
+        border-radius:50%;
+        height:50px;
+        width:50px;
+        object-fit:cover;
+        cursor:pointer;
+        transition:0.7s;
+
+        &:hover{
+            border-radius:10px;
+        }
+    }
+
+    hr{
+        border-color:black;
+        margin:auto;
+        width:30px;
+    }
 `
 
